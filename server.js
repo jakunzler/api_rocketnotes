8a48524d833837c2835fe21c353bc78a555e269d
Jonas Kunzler <k.jonasaugusto@gmail.com>
2023-05-01 22:16:20 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require("express-async-errors");
+
+const migrationsRun = require("./src/database/sqlite/migrations");
+
+const AppError = require("./src/utils/AppError");
+
+const express = require("express");
+
+const routes = require("./src/routes");
+
+migrationsRun();
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+
+    };
+
+    console.error(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
